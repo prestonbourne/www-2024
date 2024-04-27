e9f0ccb92778009a494c9478177b704619585477
saintpreston <90055250+Saintpreston@users.noreply.github.com>
2024-04-26 21:32:31 -0400

    text updates

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -21,34 +21,55 @@ export const Home = () => {
             <Main>
                 <Body>
                     I earned my Bachelor&rsquo;s degree in Design & Technology
-                    from <Link href="https://www.newschool.edu/parsons/bfa-design-technology/" target={'_blank'} >Parsons in New York </Link> . During my
-                    penultimate year, I had the privilege of studying as an
-                    exchange student at <Link href={"https://tech.cornell.edu/"} target="_blank" >Cornell University</Link>, collaborating with
-                    graduate students under the mentorship of a Ph.D. at
-                    Microsoft Azure on solutions to safeguard LLMs in regulated
-                    sectors such as Finance and Healthcare.
+                    from{" "}
+                    <Link
+                        href="https://www.newschool.edu/parsons/bfa-design-technology/"
+                        target="_blank"
+                    >
+                    Parsons School of Design
+                    </Link>
+                    &nbsp;in New York. During my penultimate year, I had the privilege of
+                    studying as an exchange student at{" "}
+                    <Link href={"https://tech.cornell.edu/"} target="_blank">
+                        Cornell University
+                    </Link>
+                    &nbsp;where I worked with MS and MBA students under a Ph.D. mentor from Microsoft Azure. We iterated on solutions to productionize LLMs
+                    in regulated sectors such as Finance and Healthcare.
                 </Body>
                 <Body>
-                    I interned at <Link href="https://www.hashicorp.com/" target="_blank" >Hashicorp</Link> and received an offer to return
-                    full-time as an Engineer on the Web Infrastructure and
-                    Platforms team.
+                    The previous summer, I interned at{" "}
+                    <Link href="https://www.hashicorp.com/" target="_blank">
+                        Hashicorp
+                    </Link>{" "}
+                    and received an offer to return as a full-time Engineer on
+                    the Web Infrastructure and Platforms team, where I work to this day.
                 </Body>
                 <Body>
-                    In tandem with my professional life, I&apos;m pursuing a
-                    Master&apos;s in Computer Science at NYU Tandon. My studies
+                    In tandem with my professional life, I&rsquo;m pursuing a
+                    Master&rsquo;s in Computer Science at NYU Tandon. My studies
                     focus on Computer rendering, perception and vision.
                 </Body>
                 <Body>
-                    You&apos;re invited to explore my written work, technical
-                    projects and social media profiles. If you have questions
+                    You&rsquo;re invited to explore my written work, technical
+                    projects and online profiles. If you have questions
                     about how I built or advice on how to improve something,
-                    don&apos;t hesitate to reach out.
+                    don&rsquo;t hesitate to reach out.
                 </Body>
+                <hr className="my-4" />
                 <Body>
-                    <hr className="my-4" />
-                    <Link href="https://www.linkedin.com/in/prestonbourne/" target="_blank">LinkedIn</Link>
+                    <Link
+                        href="https://www.linkedin.com/in/prestonbourne/"
+                        target="_blank"
+                    >
+                        LinkedIn
+                    </Link>
                     &nbsp;|&nbsp;
-                    <Link href="https://twitter.com/prestonxbourne" target="_blank">X / Twitter</Link>
+                    <Link
+                        href="https://twitter.com/prestonxbourne"
+                        target="_blank"
+                    >
+                        X / Twitter
+                    </Link>
                 </Body>
             </Main>
         </>
@@ -57,7 +78,7 @@ export const Home = () => {
 
 interface BodyProps extends React.HTMLAttributes<HTMLParagraphElement> {}
 
-const Body: React.FC<BodyProps> = ({ className, children, ...props }) => {
+const Body: React.FC<BodyProps> = ({ className = '', children, ...props }) => {
     return (
         <p
             className={`text-sm md:text-base leading-relaxed my-2 ${className}`}
