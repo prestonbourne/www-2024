17e80bfd3aa6a2be15574052be85fb36f1bf535a
Preston Bourne <preston.bourne@icloud.com>
2024-06-20 13:54:19 -0400

    use next.js metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,17 @@ import Head from "next/head";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
+import { Metadata } from "next";
 
 const firaSans = Fira_Sans({
     weight: ["400", "500", "600", "700"],
     subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+    title: "Preston Bourne | Software Engineer",
+    description: "Chasing beautiful, performant software.",
+};
 
 export default function RootLayout({
     children,
@@ -16,14 +22,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <Head>
-                <meta charSet="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="icon" href="/favicon.ico" />
-                <title>Preston Bourne | Software Engineer</title>
-                <meta name="description" content="Chasing beautiful, performant software." />
-                <meta name="og:title" content="Preston Bourne | Software Engineer" />
-            </Head>
             <body className={`${firaSans.className} bg-light dark:bg-dark text-light dark:text-dark`}>
                 {children}
                 <SpeedInsights />
