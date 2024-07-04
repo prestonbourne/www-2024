890d14d6ced87aefb2c22694b3e68d013d2613a1
Preston Bourne <preston.bourne@icloud.com>
2024-07-03 22:07:40 -0400

    shut up next.js🤫

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cx(
           firaSans.className,
