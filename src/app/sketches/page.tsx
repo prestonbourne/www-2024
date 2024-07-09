8e35378394d3b1d2b4b5c7bc4af7811c4fbc15a6
Preston Bourne <preston.bourne@icloud.com>
2024-07-09 18:56:06 -0400

    add main tag </>

--- src/app/sketches/page.tsx
+++ src/app/sketches/page.tsx
@@ -4,13 +4,13 @@ import { SketchList } from "@/components/sketches";
 
 export default async function SketchesHome() {
   return (
-    <>
+    <main>
       <ContentHeading
         title="Sketches"
         description="My explorations into visual computing, procedural, generative and interactive art. At the time of writing, this is my favorite passtime."
       />
       <Divider className="mt-2 mb-4" />
       <SketchList />
-    </>
+    </main>
   );
 }
