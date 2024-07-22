9cac26d6ed86549e932890795a3db6c3743c0ebb
Preston Bourne <preston.bourne@icloud.com>
2024-07-22 08:54:18 -0400

    reset

---
+++ src/lib/hooks/index.ts
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const useIsomorphicLayoutEffect =
+  typeof window !== 'undefined' && (window.document?.createElement || window.navigator?.product === 'ReactNative')
+    ? React.useLayoutEffect
+    : React.useEffect
