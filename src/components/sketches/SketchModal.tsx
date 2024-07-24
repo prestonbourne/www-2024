383953d4c88102a70450ddf852e49f89d9a233ca
Preston Bourne <preston.bourne@icloud.com>
2024-07-23 20:15:33 -0400

    update bg🧑‍🎨 && add click handler⚙️

--- src/components/sketches/SketchModal.tsx
+++ src/components/sketches/SketchModal.tsx
@@ -19,10 +19,23 @@ export const SketchModal: React.FC = ({}) => {
     setActiveSketch(null);
   };
 
+  const onClickOutside = (evt: React.MouseEvent<HTMLDialogElement>) => {
+    const bounds = evt.currentTarget.getBoundingClientRect();
+    if (
+      evt.clientX < bounds.x ||
+      evt.clientX > bounds.x + bounds.width ||
+      evt.clientY < bounds.y ||
+      evt.clientY > bounds.y + bounds.height
+    ) {
+      setActiveSketch(null);
+    }
+  };
+
   return (
     <AnimatePresence>
       {shouldRender && (
         <motion.dialog
+        onClick={onClickOutside}
           ref={(node) => {
             node?.showModal();
           }}
@@ -39,7 +52,7 @@ export const SketchModal: React.FC = ({}) => {
             },
           }}
           onCancel={handleCancel}
-          className="mx-auto top-48 flex flex-col w-[90vw] h-[50vh] bg-slate-100/60 dark:bg-background/65 backdrop:backdrop-blur-sm p-2 rounded-lg backdrop:bg-background/80 sheen-ring"
+          className="mx-auto top-48 flex flex-col w-[90vw] h-[50vh] bg-slate-100/90 dark:bg-background/65 backdrop:backdrop-blur-sm p-2 rounded-lg backdrop:bg-black/25 backdrop:dark:bg-background/80 sheen-ring"
         >
           <div className="ml-2 flex items-center justify-between pb-1">
             <Heading
