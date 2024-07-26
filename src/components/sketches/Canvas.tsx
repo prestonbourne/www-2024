5b3a2f30c12a22212f93eee6c4526d1b04cde27d
Preston Bourne <preston.bourne@icloud.com>
2024-07-26 17:45:23 -0400

    adjust when container is shown🧑‍🔧

--- src/components/sketches/Canvas.tsx
+++ src/components/sketches/Canvas.tsx
@@ -6,7 +6,7 @@ import { TWEAK_PANE_CONTAINER_ID } from "@/lib/sketches";
 export const Canvas = forwardRef<HTMLCanvasElement, ComponentProps<"canvas">>(
   ({ className, ...rest }, ref) => {
     return (<>
-    <div id={TWEAK_PANE_CONTAINER_ID} className="hidden absolute md:block right-4 mt-4"></div>
+    <div id={TWEAK_PANE_CONTAINER_ID} className="hidden absolute lg:block right-4 mt-4"></div>
       <canvas className={cx("w-full h-full", className)} ref={ref} {...rest} />
       </>
     );
