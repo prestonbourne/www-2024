26f123080c1edd282a29d05db9c8af4c884b53a2
Preston Bourne <preston.bourne@icloud.com>
2024-08-09 12:59:20 -0400

    try server actions

--- src/components/notes/actions.ts
+++ src/components/notes/actions.ts
@@ -1,7 +1,7 @@
 "use server"
-import { notesDAO } from "../../lib/notes/dao";
+import { notesDAO } from "@/lib/notes/dao";
 
-export const incrementViews = async (slug: string, cookies: any) => {
+export const incrementViewsAction = async (slug: string, cookies: any) => {
     console.log("server action: incrementViews");
     const res = await notesDAO.incrementViews(slug, cookies);
     return res;
