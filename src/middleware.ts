f09246742c656dda2f1d2d2275102609ab2a7211
Preston Bourne <preston.bourne@icloud.com>
2024-09-13 19:50:49 -0400

    ⚙️ `inProd` check

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,8 +8,11 @@ export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
   const workSlug = pathname.split('/').pop()
 
+ // https://vercel.com/docs/projects/environment-variables/system-environment-variables
+ const vercelEnv = process.env.VERCEL_URL
+ const inProd = vercelEnv && vercelEnv !== 'development'
+
  // because the `public` also has /work dir, exclude anything with a file extension
- const inProd = process.env.NODE_ENV === 'production'
  if (workSlug && !pathname.includes('.') && inProd) {
     incrementViewsBySlug(workSlug, createAdminSSRClient())
   }
