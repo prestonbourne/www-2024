1f9910fa60a313854d377fddb712e0307b4eca34
Preston Bourne <preston.bourne@icloud.com>
2024-09-13 19:42:50 -0400

    🎨footer margin

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -6,6 +6,7 @@ export const Divider = ({ className }: ComponentProps<"hr">) => {
     <hr
       className={cx(
         `border-t border-gray-700 dark:border-gray-500`,
+        "[mask-image:linear-gradient(to_right,transparent,black_30%,black_70%,transparent)]",
         className
       )}
     />
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,26 +1,24 @@
-import { cx } from "class-variance-authority";
-import type { ComponentProps } from "react";
-import GithubIcon from "./GithubIcon";
+import { cx } from 'class-variance-authority'
+import type { ComponentProps } from 'react'
+import GithubIcon from './GithubIcon'
+import { Divider } from '../Divider'
 
-
-export const Footer = ({ className, ...props }: ComponentProps<"footer">) => {
-  const year = String(new Date().getFullYear());
+export const Footer = ({ className, ...props }: ComponentProps<'footer'>) => {
+  const year = String(new Date().getFullYear())
 
   return (
     <footer
-      className={cx(
-        "w-full mx-auto my-4 py-2",
-        className
-      )}
+      className={cx('w-full mx-auto mb-4 mt-24 py-2', className)}
       {...props}
     >
-      <hr className="w-full mb-4 dark:border-gray-700" />
+      <Divider className={'mb-4'} />
       <div className="flex flex-row justify-between">
         <GithubIcon />
         <p className="text-sm dark:text-slate-100 text-slate-800">
-        © {year} Preston Bourne. <span className="hidden sm:inline">All Rights Reserved.</span>
+          © {year} Preston Bourne.{' '}
+          <span className="hidden sm:inline">All Rights Reserved.</span>
         </p>
       </div>
     </footer>
-  );
-};
+  )
+}
