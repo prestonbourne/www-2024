ae0f0e9b868f07a47ddfc7f3fbc349d97fab6e09
Preston Bourne <preston.bourne@icloud.com>
2024-09-13 20:48:21 -0400

    __2

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,21 +3,22 @@ import type { NextRequest } from 'next/server'
 import { incrementViewsBySlug } from './lib/work'
 import { createAgnosticAdminClient } from './lib/supabase/server-client'
 
-
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
   const workSlug = pathname.split('/').pop()
 
   /*
-  intentionally not using 
+  intentionally not using this fully
   https://vercel.com/docs/projects/environment-variables/system-environment-variables
   allows for ensuring the views increments in previews
 */
- const inProd = process.env.NODE_ENV !== 'development'
- console.log(process.env)
-
- // because the `public` also has /work dir, exclude anything with a file extension
- if (workSlug && !pathname.includes('.') && inProd) {
+  const inProd = !!process.env['VERCEL_ENV']
+  console.log({
+    'process.env': process.env,
+    inProd,
+  })
+  // because the `public` also has /work dir, exclude anything with a file extension
+  if (workSlug && !pathname.includes('.') && inProd) {
     incrementViewsBySlug(workSlug, createAgnosticAdminClient())
   }
 
