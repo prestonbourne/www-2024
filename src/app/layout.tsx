65035520b0a05d4f88db6752a710ca4d2c0cc0ee
Preston Bourne <preston.bourne@icloud.com>
2024-09-22 10:35:10 -0400

    aria-fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,62 +1,61 @@
-import { Nunito_Sans } from "next/font/google";
-import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react";
-import { Metadata } from "next";
-import { cx } from "class-variance-authority";
-import { ThemeProvider } from "@/components/providers";
-import { Flexbar } from "@/components/flexbar";
-import { Footer } from "@/components/footer";
+import { Nunito_Sans } from 'next/font/google'
+import './globals.css'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
+import { Metadata } from 'next'
+import { cx } from 'class-variance-authority'
+import { ThemeProvider } from '@/components/providers'
+import { Footer } from '@/components/footer'
+import { VercelToolbar } from '@vercel/toolbar/next'
 
 const bodyFont = Nunito_Sans({
-  subsets: ["latin"],
-  display: "swap",
-});
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://prestonbourne.dev"),
+  metadataBase: new URL('https://prestonbourne.dev'),
   title: {
-    default: "Preston Bourne | Software Engineer",
-    template: "%s | Preston Bourne",
+    default: 'Preston Bourne | Software Engineer',
+    template: '%s | Preston Bourne',
   },
-  description: "Chasing beautiful, performant software.",
+  description: 'Chasing beautiful, performant software.',
   openGraph: {
-    siteName: "Preston Bourne, Engineer & Designer",
-    url: "https://prestonbourne.dev",
+    siteName: 'Preston Bourne, Engineer & Designer',
+    url: 'https://prestonbourne.dev',
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: '/favicon.ico',
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
+  const shouldInjectToolbar = process.env.NODE_ENV === 'development'
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={cx(
           bodyFont.className,
-          "dark:text-gray-200 text-slate-800",
-          "selection:bg-secondary selection:text-purple-600 selection:bg-purple-200 dark:bg-gray-950 dark:selection:text-lime-100 dark:selection:bg-lime-800 px-4",
-          "max-w-screen-md mx-auto pt-16",
-          "min-h-screen flex flex-col", // pins the footer to the bottom
-          "relative" // for flexbar v0
-
+          'dark:text-gray-200 text-slate-800',
+          'selection:bg-secondary selection:text-purple-600 selection:bg-purple-200 dark:bg-gray-950 dark:selection:text-lime-100 dark:selection:bg-lime-800 px-4',
+          'max-w-screen-md mx-auto pt-16',
+          'min-h-screen flex flex-col', // pins the footer to the bottom
+          'relative' // for flexbar v0
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <Flexbar />
-        <div className="flex-grow">
-          {children}
-        </div>
+          <div className="flex-grow">{children}</div>
           <Footer />
         </ThemeProvider>
+        {shouldInjectToolbar && <VercelToolbar />}
         <SpeedInsights />
         <Analytics />
       </body>
     </html>
-  );
+  )
 }
