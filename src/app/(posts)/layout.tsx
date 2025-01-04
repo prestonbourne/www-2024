d2934ca735bca4143bea5416e8dc2df795a0d7ac
Preston Bourne <preston.bourne@icloud.com>
2025-01-04 17:31:16 -0500

    disable cache

--- src/app/(posts)/layout.tsx
+++ src/app/(posts)/layout.tsx
@@ -1,6 +1,10 @@
 import { Breadcrumb } from "@/components/breadcrumb";
 import React from "react";
 
+
+export const fetchCache = 'default-no-store'
+
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <React.Fragment>
